1024e9f4f35d0853091a519fdd7887469311d8ca
Jackie Leng <Jackie.Leng@nelen-schuurmans.nl>
2017-10-16 12:01:19 +0200

    Exclude tests (#15)
    * Try to exclude tests
    
    * Works after all

--- lizard_connector/__init__.py
+++ lizard_connector/__init__.py
@@ -3,4 +3,3 @@ from lizard_connector import jsdatetime
 from lizard_connector import connector
 from lizard_connector import queries
 from lizard_connector import parsers
-from lizard_connector import test
